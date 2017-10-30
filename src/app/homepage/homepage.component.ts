9246ed9a9d3b8293213082dd83f535b0857a6ffc
Michael Brooks and Mark Helt <mark@>
2017-10-30 15:09:34 -0700

    add link functionality to each post on its own page

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,30 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Post }  from '../post.model';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
+  providers: [PostService]
 })
 export class HomepageComponent implements OnInit {
+  posts: Post[];
 
-  constructor(private router: Router) { }
-
-
+  constructor(private router: Router, private postService: PostService) {}
 
   ngOnInit() {
+    this.posts = this.postService.getPosts();
   }
 
 
-  posts: Post[] = [
-    new Post('Cat attacks person', '10/29/17', 'BeesAndFacts', 'aww', 'https://www.reddit.com/r/aww/comments/79ixb5/we_keep_telling_our_friend_to_put_his/', 1),
-    new Post('Seattle Tower. Seatle\'s first art-deco tower', '10/29/17', 'vaf', 'architecture', 'https://www.reddit.com/r/architecture/comments/79okf4/seattle_tower_seattles_first_artdeco_tower_oc/', 2),
-    new Post('A Frozen Waterfall in Johnston Canyon, Banff National Park, Canada', '10/29/17', 'nickphys', 'earthporn', 'https://www.reddit.com/r/EarthPorn/comments/79nwiq/a_frozen_waterfall_in_johnston_canyon_banff/', 3),
-    new Post('The gang\'s all here!', '10/29/17', 'Killerzeit', 'telsa', 'https://imgur.com/o3ZQEGC', 4),
-    new Post('Steve Harrington - Babysitter', '10/29/17', 'Liamneeson2015', 'strangerthings', 'https://i.redd.it/tcre1evp0zuz.jpg', 5)
-
-  ];
   goToPostPage(clickedPost: Post){
     this.router.navigate(['posts', clickedPost.id]);
   };
--- src/app/postpage/postpage.component.ts
+++ src/app/postpage/postpage.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Post } from '../post.model';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-postpage',
   templateUrl: './postpage.component.html',
-  styleUrls: ['./postpage.component.css']
+  styleUrls: ['./postpage.component.css'],
+  providers: [PostService]
 })
-export class PostpageComponent implements OnInit {
 
-  constructor() { }
+export class PostpageComponent implements OnInit {
+  postId: number;
+  postToDisplay: Post;
+
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private postService: PostService
+  ) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.postId = parseInt(urlParameters['id']);
+    });
+    this.postToDisplay = this.postService.getPostById(this.postId);
   }
 
 }
