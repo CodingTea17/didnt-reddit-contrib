cdfdbb95f8fc941cbd4786fe453f802b25f753e9
Mark Helt and Michael Brooks <mark+michael@>
2017-10-30 09:45:18 -0700

    initial files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
